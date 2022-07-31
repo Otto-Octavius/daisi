033dd5735b1f431105fc589ef56d71ac0a14dfd3
Sam <sam19090@ece.ssn.edu.in>
2022-07-31 16:41:49 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,89 @@
+import numpy as np
+from PIL import Image
+
+import matplotlib.pyplot as plt
+import streamlit as st
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers, losses, optimizers, callbacks
+
+
+def build_dce_net(image_size=None) -> keras.Model:
+    input_image = keras.Input(shape=[image_size, image_size, 3])
+    conv1 = layers.Conv2D(32, (3, 3), strides=(1, 1), activation="relu", padding="same")(input_image)
+    conv2 = layers.Conv2D(32, (3, 3), strides=(1, 1), activation="relu", padding="same")(conv1)
+    conv3 = layers.Conv2D(32, (3, 3), strides=(1, 1), activation="relu", padding="same")(conv2)
+    conv4 = layers.Conv2D(32, (3, 3), strides=(1, 1), activation="relu", padding="same")(conv3)
+    int_con1 = layers.Concatenate(axis=-1)([conv4, conv3])
+    conv5 = layers.Conv2D(32, (3, 3), strides=(1, 1), activation="relu", padding="same")(int_con1)
+    int_con2 = layers.Concatenate(axis=-1)([conv5, conv2])
+    conv6 = layers.Conv2D(32, (3, 3), strides=(1, 1), activation="relu", padding="same")(int_con2)
+    int_con3 = layers.Concatenate(axis=-1)([conv6, conv1])
+    x_r = layers.Conv2D(24, (3, 3), strides=(1, 1), activation="tanh", padding="same")(int_con3)
+    return keras.Model(inputs=input_image, outputs=x_r)
+
+
+class ZeroDCE(keras.Model):
+    def __init__(self, **kwargs):
+        super(ZeroDCE, self).__init__(**kwargs)
+        self.dce_model = build_dce_net()
+
+    def get_enhanced_image(self, data, output):
+        x = data
+        for i in range(0, 3 * 8, 3):
+            r = output[:, :, :, i: i + 3]
+            x = x + r * (tf.square(x) - x)
+        return x
+
+    def call(self, data):
+        dce_net_output = self.dce_model(data)
+        return self.get_enhanced_image(data, dce_net_output)
+
+    def test_step(self, data):
+        output = self.dce_model(data)
+        return self.compute_losses(data, output)
+
+    def load_weights(self, filepath, by_name=False, skip_mismatch=False, options=None):
+        """While loading the weights, we simply load the weights of the DCE-Net"""
+        self.dce_model.load_weights(
+            filepath=filepath,
+            by_name=by_name,
+            skip_mismatch=skip_mismatch,
+            options=options,
+        )
+
+
+def get_model():
+    return ZeroDCE()
+
+
+def enhance(i):
+    model = get_model()
+    model.load_weights("OptimumW.h5")
+    image = keras.preprocessing.image.img_to_array(i)
+    image = image[:, :, :3] if image.shape[-1] > 3 else image
+    image = image.astype("float32") / 255.0
+    image = np.expand_dims(image, axis=0)
+    output_image = model(image)
+    output_image = tf.cast((output_image[0, :, :, :] * 255), dtype=np.uint8)
+    output_image = Image.fromarray(output_image.numpy())
+    return output_image
+
+
+if __name__ == '__main__':
+    st.title("Image Enhancement during Low Light Conditions")
+    user_image = st.sidebar.file_uploader("Load your own image")
+    if user_image is not None:
+        i = Image.open(user_image)
+    else:
+        i = Image.open('547.png')
+
+    draw_landmark_button = st.button('Bring out Details')
+
+    if draw_landmark_button:
+        result = enhance(i)
+        st.header("Enhanced Image")
+        st.image(result)
+    else:
+        st.header("Original Image")
+        st.image(i)
